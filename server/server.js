873a747abe88ab47fe63aa3c6e55a5d7acc62691
kpujpchoi <jpchoi@kpu.ac.kr>
2020-03-11 05:19:32 +0000

    Chap5

---
+++ server/server.js
@@ -0,0 +1,28 @@
+    const express = require('express');
+    const app = express();
+    const sensor = require('node-dht-sensor');
+
+    app.get('/temperature', function (req, res) {
+      sensor.read(11, 17, function (err, temperature, 
+    humidity) {
+        if (!err) {
+          res.send(temperature.toFixed(1) + '°C');
+        }
+      });
+    });
+
+    app.get('/humidity', function (req, res) {
+      sensor.read(11, 17, function (err, temperature, 
+    humidity) {
+        if (!err) {
+          res.send(humidity.toFixed(1) + '%');
+
+        }
+      });
+    });
+
+    app.listen(3000, function(){
+      console.log('Server listening on port 3000');
+    });
+
+
